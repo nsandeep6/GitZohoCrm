4511df5c49a20183183580356b1acab9fbd5e5e3
Nidhi-011998 <sulladmathnidhi@gmail.com>
2026-04-11 10:14:35 +0530

    First

---
+++ src/test/java/com/zoho/testscript/nidhi.java
@@ -0,0 +1,12 @@
+package com.zoho.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class nidhi {
+	@Test
+	public void createcampin() throws InterruptedException {
+		Reporter.log("created", true);
+
+}
+}
