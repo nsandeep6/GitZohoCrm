53b3ea75278df1f80e158911b1788bf1173942ba
deepthisubbanna <deepthisubbanna@gmail.com>
2026-04-13 08:58:13 +0530

    updated repo

--- src/test/java/com/zoho/testscript/Deepthi.java
+++ src/test/java/com/zoho/testscript/Deepthi.java
@@ -1,10 +1,14 @@
 package com.zoho.testscript;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 
 public class Deepthi {
 	public void test()
 	{
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
 		Reporter.log("true");
 
 }
